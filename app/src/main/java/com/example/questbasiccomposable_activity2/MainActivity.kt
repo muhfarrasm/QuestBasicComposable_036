f27b2b0e4cc25ecb39acea0ea18470eca8a4ff58
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-18 20:21:48 +0700

    Menambahkan Function image untuk menambahkan gambar

--- app/src/main/java/com/example/questbasiccomposable_activity2/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_activity2/MainActivity.kt
@@ -1,23 +1,28 @@
 package com.example.questbasiccomposable_activity2
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.questbasiccomposable_activity2.ui.theme.QuestBasicComposable_Activity2Theme
 
@@ -59,6 +64,16 @@ fun BasicCompose(farras: Modifier = Modifier) {
                 color = Color.Black
             )
         )
+
+        Image(
+            painter = painterResource(id = R.drawable.logumy),
+            contentDescription = null,
+            modifier = Modifier.size(200.dp)
+        )
+
+
+
+
     }
 
 
