03e3b0616ee866cf0175364549580f67527869d0
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-18 19:44:51 +0700

    Menambahkan fungsi BasicCompose

--- app/src/main/java/com/example/questbasiccomposable_activity2/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_activity2/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_activity2.ui.theme.QuestBasicComposable_Activity2Theme
@@ -20,9 +23,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestBasicComposable_Activity2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    BasicCompose(
+
+                        farras = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -30,12 +33,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BasicCompose(farras: Modifier = Modifier) {
+    Column (verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
+
+
 }
 
 @Preview(showBackground = true)
