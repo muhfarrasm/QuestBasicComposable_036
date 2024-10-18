a5d22ed941c4e06502c4cf5155f4328317872711
muhfarrasm <muhammadfarrasm@gmail.com>
2024-10-18 20:47:07 +0700

    Untuk membuat lengkungan di gambar

--- app/src/main/java/com/example/questbasiccomposable_activity2/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_activity2/MainActivity.kt
@@ -11,11 +11,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -94,7 +97,10 @@ fun BasicCompose(farras: Modifier = Modifier) {
         Image(
             painter = painterResource(id = R.drawable.fotosaya),
             contentDescription = null,
-            modifier = Modifier.size(260.dp)
+            modifier = Modifier
+                .size(260.dp)
+                .clip(RoundedCornerShape(260.dp))
+
         )
 
 
